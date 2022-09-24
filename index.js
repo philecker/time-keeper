12d0ee4a0e70cf154b1205606fc8ebd28fc47698
Phil Ecker <hello@philecker.com>
2022-09-23 19:29:17 -0500

    Improved speed of retrieving PTO

--- index.js
+++ index.js
@@ -3,12 +3,51 @@ const puppeteer = require('puppeteer');
 (async () => {
   try {
     // Define .env variables
-    const { url, username, password, system_id } = require('./config');
+    const { username, password, url, system_id } = require('./config');
+
+    const minimal_args = [
+      '--autoplay-policy=user-gesture-required',
+      '--disable-background-networking',
+      '--disable-background-timer-throttling',
+      '--disable-backgrounding-occluded-windows',
+      '--disable-breakpad',
+      '--disable-client-side-phishing-detection',
+      '--disable-component-update',
+      '--disable-default-apps',
+      '--disable-dev-shm-usage',
+      '--disable-domain-reliability',
+      '--disable-extensions',
+      '--disable-features=AudioServiceOutOfProcess',
+      '--disable-hang-monitor',
+      '--disable-ipc-flooding-protection',
+      '--disable-notifications',
+      '--disable-offer-store-unmasked-wallet-cards',
+      '--disable-popup-blocking',
+      '--disable-print-preview',
+      '--disable-prompt-on-repost',
+      '--disable-renderer-backgrounding',
+      '--disable-setuid-sandbox',
+      '--disable-speech-api',
+      '--disable-sync',
+      '--hide-scrollbars',
+      '--ignore-gpu-blacklist',
+      '--metrics-recording-only',
+      '--mute-audio',
+      '--no-default-browser-check',
+      '--no-first-run',
+      '--no-pings',
+      '--no-sandbox',
+      '--no-zygote',
+      '--password-store=basic',
+      '--use-gl=swiftshader',
+      '--use-mock-keychain',
+    ];
 
     // Open new browswer and set timeout
     const browser = await puppeteer.launch({
       headless: true,
-      devtools: false
+      devtools: false,
+      args: minimal_args
     })
 
     const page = await browser.newPage()
@@ -26,11 +65,11 @@ const puppeteer = require('puppeteer');
     ]);
 
     // await navigating to Leave Status page
-    await page.click('#goToLbl', { delay: 10000 })
-    await page.click('#bus__PE', { delay: 5000 })
-    await page.click('#dpt__ES', { delay: 5000 })
-    await page.click('#wrk__PayrollandBenefits', { delay: 5000 })
-    await page.click('#actvty__ESQLVSTAT', { delay: 5000 })
+    await page.click('#goToLbl', { delay: 5000 })
+    await page.click('#bus__PE', { delay: 2500 })
+    await page.click('#dpt__ES', { delay: 2500 })
+    await page.click('#wrk__PayrollandBenefits', { delay: 2500 })
+    await page.click('#actvty__ESQLVSTAT', { delay: 2500 })
 
     // await Leave Status input
     await page.waitForSelector('#CURRENT_BAL-_0_E')
